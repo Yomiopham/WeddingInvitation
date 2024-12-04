f62553ab3c8955ea8cb7ac24abdeb273f900f905
HoanPhamSJSU <69431005+HoanPhamSJSU@users.noreply.github.com>
2024-12-03 21:48:52 -0600

    Commit
    Commit

---
+++ out/_next/static/chunks/pages/product-single/alltab-90a74f7aeb7cd161.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1238],{60113:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/product-single/alltab",function(){return u(60286)}])}},function(n){n.O(0,[7461,286,9774,2888,179],function(){return n(n.s=60113)}),_N_E=n.O()}]);
